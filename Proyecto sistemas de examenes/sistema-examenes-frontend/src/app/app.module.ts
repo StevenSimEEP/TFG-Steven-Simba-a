acb591bc1601e6cdde8c78364cdc4a7f30acbdd8
DeblaDaw <145448953+StevenSimEEP@users.noreply.github.com>
2025-03-02 21:48:36 +0100

    Función de carga
    Se añadió una pantalla de carga al momento de de cambiar de url.

--- Proyecto sistemas de examenes/sistema-examenes-frontend/src/app/app.module.ts
+++ Proyecto sistemas de examenes/sistema-examenes-frontend/src/app/app.module.ts
@@ -42,6 +42,7 @@ import { SidebarComponent as UserSidebar } from './pages/user/sidebar/sidebar.co
 import { LoadExamenComponent } from './pages/user/load-examen/load-examen.component';
 import { InstruccionesComponent } from './pages/user/instrucciones/instrucciones.component';
 import { StartComponent } from './pages/user/start/start.component';
+import { NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -84,7 +85,11 @@ import { StartComponent } from './pages/user/start/start.component';
     MatListModule,
     MatSlideToggleModule,
     MatSelectModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule.forRoot({
+      showForeground:true
+    })
   ],
   providers: [
     provideClientHydration(),
